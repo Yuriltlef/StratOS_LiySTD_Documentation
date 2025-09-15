62d2f3ecdb65c19e7f9eecc1cd868552e36a72c9
yurilt_s <33693@qq.com>
2025-09-15 09:03:21 +0800

    测试

--- docs/js/theme-loader.js
+++
@@ -1,9 +0,0 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/StratOS_LiySTD_Documentation/js/sw.js').then(function(registration) {
-      console.log('SW registered: ', registration);
-    }).catch(function(registrationError) {
-      console.log('SW registration failed: ', registrationError);
-    });
-  });
-}
